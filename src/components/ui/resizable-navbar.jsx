3588176d7bc65fe02fdd2b424d343f99f3b38497
Darryl Mathias <mathiasndarryl7@gmail.com>
2025-06-15 13:11:46 +0530

    Updated Navbar Logo size

--- src/components/ui/resizable-navbar.jsx
+++ src/components/ui/resizable-navbar.jsx
@@ -192,7 +192,7 @@ export const NavbarLogo = () => {
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
       <img src="/Logo.png" alt="logo" width={30} height={30} />
-      <span className="font-normal text-black dark:text-white text-lg">
+      <span className="font-normal text-black dark:text-white text-sm sm:text-md">
         Darryl Mathias
       </span>
     </a>
