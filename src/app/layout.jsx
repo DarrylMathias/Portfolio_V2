e3bc471b86b3e1fe1b3d128c90f1dd52165ba49e
Darryl Mathias <mathiasndarryl7@gmail.com>
2025-06-16 21:44:18 +0530

    Added Vercel Speed Insights

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SpeedInsights/>
         <Analytics />
         <ThemeProvider
           attribute="class"
