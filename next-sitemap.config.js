31c82bf33db03a61d82a821b388c9e7b587482e2
Darryl Mathias <mathiasndarryl7@gmail.com>
2025-06-18 16:38:04 +0530

    SEO optimized

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://darrylmathias.vercel.app",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  changefreq: "monthly",
+  priority: 0.7,
+  sitemapSize: 5000,
+};
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -16,8 +16,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Darryl's Portfolio",
-  description: "Modern & Minimalist Portfolio",
+  title: "Darryl Mathias | Full-Stack Developer & Software Engineer Portfolio",
+  description: "Explore the modern, minimalist portfolio of Darryl Mathias — a passionate full-stack developer and software engineer. Showcasing projects, skills, and experience in web development, React, and more.",
 };
 
 export default function RootLayout({
@@ -25,6 +25,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="Explore the modern, minimalist portfolio of Darryl Mathias — a passionate full-stack developer and software engineer. Showcasing projects, skills, and experience in web development, React, and more." />
+  <meta name="keywords"
+    content="darryl, darryl mathias, mathias, home, skills, projects, contacts, web development, development website, web designing, web designing websites, web designing web, Darryl portfolio, Portfolio website, html css projects" />
+  <meta name="author" content="Darryl Mathias" />
+  <link rel="canonical" href="https://darrylmathias.vercel.app" />
+</head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
